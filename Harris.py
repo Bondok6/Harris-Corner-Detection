5a0571704cd14ad940a358c44e0fec5d221068ba
Bondok6 <kirolousbondok6@gmail.com>
2021-05-19 03:18:22 +0200

    add some useful comments

--- Harris.py
+++ Harris.py
@@ -19,9 +19,16 @@ R = cv.cornerHarris(img_gray, 2, 3, 0.04)
 R = cv.dilate(R, None)
 
 # Threshold for an optimal value, it may vary depending on the image. (interest point)
+# طالما اكبر من واحد ف الميه من الماكس تبقي كونر
+# R is large & R > 0 ,then it's a Corner
 img[R > 0.01 * R.max()] = [0, 0, 255]
 
 cv.imshow('Output', img)
 
 if cv.waitKey(0):
     cv.destroyAllWindows()
+
+
+# References:
+# https://titanwolf.org/Network/Articles/Article?AID=e0121078-7654-4b48-8d03-6bdde54f1b58#gsc.tab=0
+# https://www.youtube.com/watch?v=KH8Mq9FPVPw&t=41s
