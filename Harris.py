c9fa9e759cb1861bf0d7ee381184c19b5617bf67
Bondok6 <kirolousbondok6@gmail.com>
2021-05-19 03:13:50 +0200

    add some useful comments

--- Harris.py
+++ Harris.py
@@ -2,18 +2,26 @@ import numpy as np
 import cv2 as cv
 
 img = cv.imread('chess_board.png')
+img = cv.resize(img, (400, 400))
+cv.imshow('Input', img)
 
-cv.imshow('img', img)
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+img_gray = np.float32(img_gray)
 
-gray = np.float32(gray)
-dst = cv.cornerHarris(gray, 2, 3, 0.04)
+# (img, blockSize, k-size, k)
+# img -> must be gray and float32
+# block size -> size of Window (corner detection field size to be considered)
+# k size -> Aperture parameter of sobel derivative used
+# k -> Harris corner detection of free parameters in the equation, the value for the parameter [0,04,0.06].
+R = cv.cornerHarris(img_gray, 2, 3, 0.04)
 
-dst = cv.dilate(dst, None)
+# to enhance subsequent expansion code is marked clear image corners optional accuracy may be commented (NOt Important )
+R = cv.dilate(R, None)
 
-img[dst > 0.01 * dst.max()] = [0, 0, 255]
+# Threshold for an optimal value, it may vary depending on the image. (interest point)
+img[R > 0.01 * R.max()] = [0, 0, 255]
 
-cv.imshow('dst', img)
+cv.imshow('Output', img)
 
 if cv.waitKey(0):
     cv.destroyAllWindows()
